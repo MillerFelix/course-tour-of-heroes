039b5877df0c2a8a49752d67a81e52826ee0c559
Miller <millerredigolo@gmail.com>
2023-07-10 15:06:22 -0300

    Implementing list with heroes.

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero.model';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Negan' },
+  { id: 2, name: 'Rick' },
+  { id: 3, name: 'Carl' },
+  { id: 4, name: 'Daryl' },
+  { id: 5, name: 'Carol' },
+  { id: 6, name: 'Michone' },
+  { id: 7, name: 'Shane' },
+  { id: 8, name: 'Maggie' },
+  { id: 9, name: 'Gleen' },
+  { id: 10, name: 'Rosita' },
+];
